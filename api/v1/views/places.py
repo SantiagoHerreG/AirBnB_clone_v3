53e201f00f381f5021cd1a97351c521b83005581
SantiagoHerreG <888@holbertonschool.com>
2020-02-16 19:48:52 +0000

    view places: added new route to search places, using a post request. Retrieves places by states, cities or amenities

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,7 +8,61 @@ from models import storage
 from models.city import City
 from models.place import Place
 from models.user import User
+from models.state import State
 from flask import request
+
+
+@app_views.route("/places_search", methods=["POST"])
+def places_search():
+    """List Place objects depending on a Json request
+    """
+    new_obj = request.get_json()
+    if new_obj is None:
+        abort(400, "Not a JSON")
+
+    city_list = []
+    if "cities" in new_obj.keys():
+        city_list = new_obj["cities"]
+
+    if "states" in new_obj.keys():
+        states_list = new_obj["states"]
+        for id in states_list:
+            state = storage.get(State, id)
+            if state is None:
+                pass
+            for city in state.cities:
+                if city.id not in city_list:
+                    city_list.append(city.id)
+
+    amenities_list = []
+    if "amenities" in new_obj.keys():
+        amenities_list = new_obj["amenities"]
+
+    if len(new_obj) == 0 or (len(city_list) == 0 and len(amenities_list) == 0):
+        list_res = []
+
+        all_places = storage.all(Place)
+        for place in all_places.values():
+            list_res.append(place.to_dict())
+        return jsonify(list_res)
+
+    list_res = []
+    for city_id in city_list:
+        city = storage.get(City, city_id)
+        if city is None:
+            continue
+        places = city.places
+        for place in places:
+            ame_in_place = []
+            for ame in place.amenities:
+                ame_in_place.append(ame.id)
+            del place.amenities
+            list_res.append(place.to_dict())
+            for amenity_id in amenities_list:
+                if amenity_id not in ame_in_place:
+                    list_res.remove(place.to_dict())
+
+    return jsonify(list_res)
 
 
 @app_views.route("/cities/<city_id>/places", methods=["POST"],
@@ -33,10 +87,10 @@ def new_place(city_id):
         abort(400, "Missing name")
 
     new_obj["city_id"] = city_id
-    new_user = Place(**new_obj)
-    new_user.save()
+    new_place = Place(**new_obj)
+    new_place.save()
     storage.reload()
-    return jsonify(new_user.to_dict()), 201
+    return jsonify(new_place.to_dict()), 201
 
 
 @app_views.route("/places/<place_id>", methods=["PUT"], strict_slashes=False)
